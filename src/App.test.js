717de8b843ea77546384b49bb3c0d558e9042b52
wuilmer carbajal <wcarbajal@outlook.com>
2026-03-20 00:51:11 -0500

    luego del mock

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,8 @@ import App from './App';
 
 test( 'muestra Hola mundo en la página', () => {
   render( <App /> );
+  //console.log( render() );
   const elemento = screen.getByText( /hola mundo/i );
+
   expect( elemento ).toBeInTheDocument();
 } );
